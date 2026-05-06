f8682dafd8673f8f5061ae5735b806788cc6c5ff
Michas <michaelcalb.dev@gmail.com>
2026-05-06 14:20:46 -0300

    proxy update
    now proxy always prioritize the maintenance mode check

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,16 +3,16 @@ import { NextRequest } from "next/server";
 
 export function proxy(request: NextRequest) {
     
-    /* CTF LOCK */
-    if (request.nextUrl.pathname.startsWith('/ctf') && request.cookies.get('ctf_unlocked')?.value !== 'true') {
-        return NextResponse.rewrite(new URL('/not-found', request.url))
-    }
-
     /* MAINTENANCE MODE */
     const isMaintenanceMode = process.env.MAINTENANCE_MODE === 'true'
 
     if (isMaintenanceMode && !request.nextUrl.pathname.startsWith('/maintenance')) {
         return NextResponse.rewrite(new URL('/maintenance', request.url))
+    }
+
+    /* CTF LOCK */
+    if (request.nextUrl.pathname.startsWith('/ctf') && request.cookies.get('ctf_unlocked')?.value !== 'true') {
+        return NextResponse.rewrite(new URL('/not-found', request.url))
     }
 
     return NextResponse.next()
